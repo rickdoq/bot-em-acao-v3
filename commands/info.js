ab263cf1679d0679490fa26fa379a16d573775fd
rickdoq <64667029+rickdoq@users.noreply.github.com>
2020-06-19 12:07:32 -0300

    update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,35 +2,11 @@ const Discord = require("discord.js");
 const db = require("quick.db");
 
 module.exports.run = async (bot, message, args) => {
-  if (!message.content.startsWith("!!")) return;
-
-  let user = message.mentions.members.first() || message.author;
-
-  let money = await db.fetch(`money_${message.guild.id}_${user.id}`);
-  if (money === null) money = 0;
-
-  let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`);
-  if (bank === null) bank = 0;
-
-  let vip = await db.fetch(`bronze_${message.guild.id}_${user.id}`);
-  if (vip === null) vip = "None";
-  if (vip === true) vip = "Bronze";
-
-  let shoes = await db.fetch(`nikes_${message.guild.id}_${user.id}`);
-  if (shoes === null) shoes = "0";
-
-  let newcar = await db.fetch(`car_${message.guild.id}_${user.id}`);
-  if (newcar === null) newcar = "0";
-
-  let newhouse = await db.fetch(`house_${message.guild.id}_${user.id}`);
-  if (newhouse === null) newhouse = "0";
-
-  let moneyEmbed = new Discord.MessageEmbed()
-    .setColor("#FFFFFF")
-    .setDescription(
-      `**${user} Info**\n\nCarteira: ${money}\nBanco: ${bank}\nVIP Rank: ${vip}\n\n**Inventory**\n\n----: ${shoes}\nCarros: ${newcar}\nMansões: ${newhouse}`
-    );
-  message.channel.send(moneyEmbed);
+  const embed = new Discord.MessageEmbed()
+.setTitle(`Badges do Perfil`)
+.setDescription(`Talvez muitos já tenham visto que no perfil aparece umas badges, porém muitos não sabem o significado.\nEntão estou trazendo aqui o significado de todas as badges:\n\n**<:capitao:723269895856586799> - Este badge é para quem tem o cargo de Capitão.\n<:tcoronel:723270160143614023> - Este badge é para quem tem o cargo de Tenente-Coronel.\n<:coronel:723270318629847060> - Este badge é para quem tem o cargo Coronel.\n\`Easter Egg\` - Este badge é um Easter Egg.\n<:sub:723195844509302805> - Este badge é para quem é Sub na Twitch.\n<:nitroperfil:723195981121716325> - Este badge é para quem der nitro no servidor.\n<:bolsa:723194962350440449> - Este badge é para quem realizou o registro do Bolsa Família.\n<:bughunter:723195052922503200> - Este badge é para quem reportou um Bug válido.\n<:dev:723195147504058439> - Este badge é para quem é desenvolvedor do bot.\n👮 - Este badge é para quem é Staff.**`)
+.setColor(`DARK`);
+message.channel.send(embed)
 };
 
 module.exports.help = {
